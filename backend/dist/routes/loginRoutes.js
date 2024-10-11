be683674486eb0e4ab0d9c26fa758758df6e7cd3
Raissa <raissa.vilemon@gmail.com>
2024-10-11 11:49:53 -0300

    feat: adiciona o cors no backend

--- backend/dist/routes/loginRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const loginController_1 = require("../controllers/loginController");
-const router = (0, express_1.Router)();
-router.post("/", loginController_1.authenticate);
-exports.default = router;
