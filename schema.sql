30cde07f8fb3e45c36b1884dfefed41914b957b2
Raissa <raissa.vilemon@gmail.com>
2024-10-14 13:15:35 -0300

    feat: instalação das dependências para langchain e ajuste da tabela notes

--- schema.sql
+++ schema.sql
@@ -17,16 +17,30 @@ CREATE TABLE IF NOT EXISTS notes (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
-    -- type TEXT,
-    -- url TEXT,
     embedding VECTOR(1536),
+    metadata JSONB, 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_by UUID NOT NULL,
-    updated_by UUID NOT NULL,
-    CONSTRAINT fk_created_by FOREIGN KEY (created_by) REFERENCES users (id),
-    CONSTRAINT fk_updated_by FOREIGN KEY (updated_by) REFERENCES users (id)
+    CONSTRAINT fk_created_by FOREIGN KEY (created_by) REFERENCES users (id)
 );
+
+
+-- A indexação vai ajudar a otimizar as buscas, pois pode chegar em um ponto que existe muitas notas para serem analisadas
+CREATE INDEX ON notes USING ivfflat (embedding VECTOR_COSINE_OPS)
+WITH (lists = 100);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- Cria a tabela de imagens associadas às notas
