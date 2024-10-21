c3c94431e07562aa2ac4d6b97498584e44490b5b
Pedro Lucas Fernandes <plff201100@gmail.com>
2024-10-21 01:00:16 -0300

    feat: add url route

---
+++ backend/src/routes/urlRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as urlController from '../controllers/urlController';
+import { auth } from '../middlewares/auth';
+
+
+const router = Router();
+
+// Rota para adicionar URL
+router.post('/', auth, urlController.addUrl);
+
+// Rota para remover uma URL (se necessário)
+router.delete('/:imageId', auth, urlController.deleteUrl);
+
+export default router;
