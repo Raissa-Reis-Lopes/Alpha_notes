e8e70157b5065a151793da4b49a70fd6a8317d0e
Maiza Ferreira <maizafcontato@gmail.com>
2024-10-23 02:02:43 -0300

    feat: logout ; style: ajuste na pagina de login e cadastro

--- frontend/src/components/TrashNoteList/TrashNoteList.tsx
+++ frontend/src/components/TrashNoteList/TrashNoteList.tsx
@@ -1,9 +1,6 @@
-
 import { Box } from "@mui/material";
-import { DescriptionOutlined } from '@mui/icons-material';
-import { Typography } from '@mui/joy';
 import TrashNoteCard from '../TrashNoteCard/TrashNoteCard';
-import { Note, useNotes } from '../../contexts/NotesContext';
+import { Note } from '../../contexts/NotesContext';
 
 interface TrashNoteListProps {
   notes: Note[];
@@ -12,38 +9,24 @@ interface TrashNoteListProps {
 const TrashNoteList: React.FC<TrashNoteListProps> = ({ notes }) => {
   return (
     <Box className="trash-note-list-component">
-      {!notes || notes.length === 0 ? (
-        <Box sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          marginTop: "10vh"
-        }}>
-          <DescriptionOutlined sx={{ fontSize: "100px", opacity: "0.1" }} />
-          <Typography sx={{ fontSize: "24px", color: "#5f6368" }}>Nenhuma anotação encontrada na lixeira.</Typography>
-        </Box>
-      ) : (
-        <Box sx={{
-          display: "flex",
-          flexDirection: 'column',
-          gap: "16px",
-        }}>
-          {notes.map(item => (
-            <TrashNoteCard
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              content={item.content}
-              date={(new Date(item.date).toLocaleString())}
-              archived={item.archived}
-              metadata={item.metadata}
-              status={item.status}
-            />
-          ))}
-        </Box>
-      )}
+      <Box sx={{
+        display: "flex",
+        flexDirection: 'column',
+        gap: "16px",
+      }}>
+        {notes.map(item => (
+          <TrashNoteCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            content={item.content}
+            date={(new Date(item.date).toLocaleString())}
+            archived={item.archived}
+            metadata={item.metadata}
+            status={item.status}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
